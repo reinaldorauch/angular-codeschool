dcd916b043bd084566ce567ba3050edefe03464f
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-02-26 00:08:37 -0300

    Section 1.4 do curso

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', []);
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+
+    var gem = {
+        name: "Dodecaedro",
+        price: 2.85,
+        description: 'E um Dodecaedro'
+    };
+
+})();
