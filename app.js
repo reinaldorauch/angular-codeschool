38179796d144fe8207c81713cf465c9ca3873094
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-02-27 09:14:42 -0300

    Adicionado controller para a lista de progressbar e generalizado caso de incrementos e decrementos

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
+var gems = [
+    {
+        name: "Dodecaedro",
+        price: 2.85,
+        description: 'E um Dodecaedro',
+        canPurchase: true,
+        soldOut: false
+    },
+    {
+        name: "Pentagonal",
+        price: 10.00,
+        description: 'Pentagonal, not USA',
+        canPurchase: false,
+        soldOut: false
+    }
+];
+
+var list = [1, 2, 3, 4, 5, 6, 7];
+
 (function() {
     var app = angular.module('store', []);
 
     app.controller('StoreController', function () {
         this.products = gems;
     });
-
-    var gems = [
-        {
-            name: "Dodecaedro",
-            price: 2.85,
-            description: 'E um Dodecaedro',
-            canPurchase: true,
-            soldOut: false
-        },
-        {
-            name: "Pentagonal",
-            price: 10.00,
-            description: 'Pentagonal, not USA',
-            canPurchase: false,
-            soldOut: false
-        }
-    ];
 
     app.controller('PanelController', function () {
         this.tab = 1;
@@ -36,23 +38,30 @@
 
     var progress = angular.module('progress', []);
 
+    progress.controller("ProgressListController", function () {
+        this.progressList = list;
+    });
+
     progress.controller('ProgressController', function ($interval) {
         var _this = this;
 
         _this.size = 1;
+        _this.min  = 0;
+        _this.max  = 100;
 
         this.increase = function (i) {
-            _this.size = _this.size < 100 ? _this.size + (i || 1) : _this.size;
+            i = (i + _this.size) > _this.max ? _this.max - _this.size : i;
+            _this.size = _this.size < _this.max ? _this.size + (i || 1) : _this.size;
         };
 
         this.decrease = function (d) {
-            _this.size = _this.size > 0 ? _this.size - (d || 1) : _this.size;
+            i = (_this.size - i) < _this.min ? _this.size : i;
+            _this.size = _this.size > _this.min ? _this.size - (d || 1) : _this.size;
         };
 
         var interval = $interval(function () {
-            console.log('INcreasing');
-            _this.increase();
-            if(_this.size === 100)
+            _this.increase(Math.round(Math.random() * 10));
+            if(_this.size === _this.max)
                 $interval.cancel(interval);
         }, 1000);
     });
