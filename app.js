99ec59c52095d167c41819a00edb01587462f3c9
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-02-26 21:15:51 -0300

    Adicionado controller para progressbar

--- app.js
+++ app.js
@@ -22,4 +22,40 @@
         }
     ];
 
+    app.controller('PanelController', function () {
+        this.tab = 1;
+
+        this.selectTab = function (tab) {
+           this.tab = tab;
+        };
+
+        this.isSelected = function (tab) {
+            return this.tab === tab;
+        };
+    });
+
+    var progress = angular.module('progress', []);
+
+    progress.controller('ProgressController', function ($interval) {
+        this.size = 1;
+
+        this.increase = function (i) {
+            this.size = this.size < 100 ? this.size + (i || 1) : this.size;
+            console.log(this.size);
+        };
+
+        this.decrease = function (d) {
+            this.size = this.size > 0 ? this.size - (d || 1) : this.size;
+        };
+
+        var _this = this;
+
+        var interval = $interval(function () {
+            console.log('INcreasing');
+            _this.increase();
+            if(_this.size === 100)
+                $interval.cancel(interval);
+        }, 1000);
+    });
+
 })();
