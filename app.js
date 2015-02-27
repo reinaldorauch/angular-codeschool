415758b1dcf857585fc4366db284ec8aed896292
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-02-27 10:23:21 -0300

    Adicionado gambiarras para atualizar a média das barras no topo

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ var gems = [
     }
 ];
 
-var list = [1, 2, 3, 4, 5, 6, 7];
-
 (function() {
     var app = angular.module('store', []);
 
--- progress.js
+++ progress.js
@@ -1,16 +1,67 @@
 (function() {
     var progress = angular.module('progress', []);
 
-    progress.controller("ProgressListController", function () {
-        this.progressList = list;
+    progress.service('updAvg', function () {
+        var _this = this,
+            _list;
+
+        Object.defineProperties(this, {
+            list: {
+                set: function (l) {
+                    _list = l;
+                }
+            }
+        });
+
+        this.avg = 0;
+
+        this.updateAvg = function (pl) {
+            var avg = 0;
+
+            _list.forEach(function (v, i) {
+                avg += v.size;
+            });
+
+            _this.avg = avg / _list.length;
+
+            pl.avg = _this.avg;
+        }
+
+        this.updateList = function (val, i) {
+            _list[i].size = val;
+        };
     });
 
-    progress.controller('ProgressController', function ($interval) {
+    progress.controller("ProgressListController", ['updAvg', function (updAvg) {
         var _this = this;
 
-        _this.size = 1;
-        _this.min  = 0;
-        _this.max  = 100;
+        this.progressList = list;
+
+        updAvg.list = list;
+
+        updAvg.updateAvg(this);
+    }]);
+
+    progress.controller('ProgressController', ['$interval', 'updAvg', function ($interval, updAvg) {
+        var _this = this,
+            _size,
+            _plc;
+
+        Object.defineProperties(this, {
+            size: {
+                get: function() { return _size; },
+                set: function (v) {
+                    updAvg.updateList(v, _this.index);
+                    if(_this.plc)
+                        updAvg.updateAvg(_this.plc);
+                    return _size = v;
+                }
+            }
+        });
+
+        this.index = 0;
+        this.min  = 0;
+        this.max  = 100;
 
         this.increase = function (i) {
             i = (i + _this.size) > _this.max ? _this.max - _this.size : i;
@@ -27,6 +78,6 @@
             if(_this.size === _this.max)
                 $interval.cancel(interval);
         }, 1000);
-    });
+    }]);
 
 })();
