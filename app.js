4128de77d09906914d1ab449494fb3406b4aefc3
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2015-02-28 03:16:45 -0300

    Finalizado curso de angularjs

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-var gems = [
-    {
-        name: "Dodecaedro",
-        price: 2.85,
-        description: 'E um Dodecaedro',
-        canPurchase: true,
-        soldOut: false
-    },
-    {
-        name: "Pentagonal",
-        price: 10.00,
-        description: 'Pentagonal, not USA',
-        canPurchase: false,
-        soldOut: false
-    }
-];
-
 (function() {
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
+
+    app.controller('StoreController', ['$http', '$log', function ($http, $log) {
+        var _this = this;
+
+        this.products = [];
+
+        $http.get('products.json')
+            .success(function (data) {
+                $log.info('success request');
+                $log.info(data);
+                _this.products = data;
+            })
+            .error(function (error) {
+                $log.error('Error with the request');
+                $log.error(error);
+            });
+    }]);
+
+    app.controller("ReviewController", function() {
+        var _this = this;
+
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(_this.review);
+            this.review = {};
+        }
     });
 
-    app.controller('PanelController', function () {
-        this.tab = 1;
-
-        this.selectTab = function (tab) {
-           this.tab = tab;
-        };
-
-        this.isSelected = function (tab) {
-            return this.tab === tab;
-        };
-    });
 })();
